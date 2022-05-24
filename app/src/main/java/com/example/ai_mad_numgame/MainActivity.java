9cf3fd1af36a8257ef21d1c36c5120eab6bf68ed
ishita <ishita126jain@gmail.com>
2022-05-24 12:46:29 +0530

    add more functionality

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -72,9 +72,48 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
-        //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
+        int operand2= random.nextInt(10);
+        int correctAnswer = -100;
         String operator = operators[random.nextInt(4)];
+        correctButton = random.nextInt(4);
+        if(operator.equals("+")){
+            correctAnswer = operand1 + operand2;
+        }
+        else if(operator.equals("-")){
+            correctAnswer = operand1 - operand2;
+        }
+        else if(operator.equals("*")){
+            correctAnswer = operand1 * operand2;
+        }
+        else{
+            correctAnswer = operand1 / operand2;
+        }
+        if(correctButton == 0){
+            button1.setText(correctAnswer + "");
+            button2.setText(correctAnswer +1 + "");
+            button3.setText(correctAnswer -1 +"");
+            button4.setText(correctAnswer + 2 +"");
+        }
+        if(correctButton == 1){
+            button1.setText(correctAnswer + "");
+            button2.setText(correctAnswer +1 + "");
+            button3.setText(correctAnswer -1 +"");
+            button4.setText(correctAnswer + 2 +"");
+        }
+        if(correctButton == 2){
+            button1.setText(correctAnswer + "");
+            button2.setText(correctAnswer +1 + "");
+            button3.setText(correctAnswer -1 +"");
+            button4.setText(correctAnswer + 2 +"");
+        }
+        if(correctButton == 3){
+            button1.setText(correctAnswer + "");
+            button2.setText(correctAnswer +1 + "");
+            button3.setText(correctAnswer + -1 +"");
+            button4.setText(correctAnswer + 2 +"");
+        }
+        //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
+
         textView2.setText(operand1 + operator + operand2);
 
       // Your code here, to diplay correct and incorrect options on the buttons
